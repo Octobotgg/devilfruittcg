354048cfe521fb6b66d64ada6b51ab3bac612345
Javier Barro <javierbarro@Javiers-MacBook-Pro.local>
2026-03-01 21:32:56 -0500

    fix: include OP11-OP14 in market search seed cards

--- lib/cards.ts
+++ lib/cards.ts
@@ -8,6 +8,10 @@ import OP07_CARDS from './op07-cards';
 import OP08_CARDS from './op08-cards';
 import OP09_CARDS from './op09-cards';
 import OP10_CARDS from './op10-cards';
+import OP11_CARDS from './op11-cards';
+import OP12_CARDS from './op12-cards';
+import OP13_CARDS from './op13-cards';
+import OP14_CARDS from './op14-cards';
 import EB01_CARDS from './eb01-cards';
 import EB02_CARDS from './eb02-cards';
 import ST01_CARDS from './st01-cards';
@@ -58,6 +62,10 @@ export const SEED_CARDS: Card[] = [
   ...OP08_CARDS,
   ...OP09_CARDS,
   ...OP10_CARDS,
+  ...OP11_CARDS,
+  ...OP12_CARDS,
+  ...OP13_CARDS,
+  ...OP14_CARDS,
   ...EB01_CARDS,
   ...EB02_CARDS,
   ...ST01_CARDS,
@@ -177,6 +185,10 @@ export const SETS = [
   { code: "OP08", name: "Two Legends" },
   { code: "OP09", name: "Emperors in the New World" },
   { code: "OP10", name: "Royal Blood" },
+  { code: "OP11", name: "Set OP11" },
+  { code: "OP12", name: "Set OP12" },
+  { code: "OP13", name: "Set OP13" },
+  { code: "OP14", name: "Set OP14" },
   { code: "EB01", name: "Memorial Collection" },
   { code: "EB02", name: "Anime 25th Collection" },
   { code: "ST01", name: "Straw Hat Crew" },
